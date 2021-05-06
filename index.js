8b5b4f8e2f0851fa9b59586a64a4aede82df5276
Emmanuel <emmanuelamon92@gmail.com>
2021-05-06 08:45:52 -0400

    commit tree a67ada7242281a9700a0d3338f03c53b9f4ca77e

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function superbowlWin(record){
+    for (const year of record){
+        // console.log(year)
+        for (const key in year){
+            if(year[key] === 'W'){
+                // console.log(year.year)
+                return year.year
+            }
+        }
+    }
+}
 
+console.log(record.find(superbowlWin));
